5df3d270fabde6782bcd1ffb5abd117ad6544e33
sabrina16 <sabrinatoubbeh@gmail.com>
2018-11-19 00:00:31 -0800

    working numbers.sh

---
+++ numbers.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+# numbers.sh
+# Sabrina Toubbeh
+
+
+COUNT=1
+
+echo "Enter a positive number: "
+read NUMBER
+
+while [ $COUNT -le $NUMBER  ]
+do
+	if [ $((COUNT%2)) -eq 0 ]
+	then
+		echo "$COUNT. EVEN"
+	else
+		echo "$COUNT. ODD"
+	fi
+
+	COUNT=$((COUNT+1))
+done
